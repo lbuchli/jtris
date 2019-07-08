85d37c3d5a8330ac9a18990ede659b7eaac89268
Lukas Buchli <lbu01@buchli-bigger.ch>
2019-07-08 13:27:05 +0200

    Begin drawing next block

--- src/ch/vfl/jtris/game/Next.java
+++ src/ch/vfl/jtris/game/Next.java
@@ -1,6 +1,7 @@
 package ch.vfl.jtris.game;
 
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 
 class Next implements IBlockFeeder {
     private static final int POLYOMINO_SIZE = 4;
@@ -8,16 +9,26 @@ class Next implements IBlockFeeder {
     private Block next;
 
     private Canvas canvas;
+    private GraphicsContext graphicsContext;
 
     Next(Canvas next) {
         this.canvas = next;
+        this.graphicsContext = this.canvas.getGraphicsContext2D();
     }
 
     public Block generateNext() {
         next = new Block(POLYOMINO_SIZE);
-
-        // TODO draw block on pane
+        drawNextBlock();
 
         return next;
+    }
+
+    // drawNextBlock draws the next block onto the canvas via the graphicsContext.
+    private void drawNextBlock() {
+        boolean[][] shape = next.getShape();
+        double squareWidth = canvas.getWidth() / shape.length;
+        double squareHeight = canvas.getHeight() / shape.length;
+
+
     }
 }
